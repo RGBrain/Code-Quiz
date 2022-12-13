2e033da8ab2b75d97ac62c10351c6edf11ac392c
RGBrain <31857067+RGBrain@users.noreply.github.com>
2022-12-13 19:06:58 +0000

    WIP

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,7 +4,11 @@ var startScreen = document.querySelector('#start-screen');
 var feedback = document.getElementById("feedback");
 var timer = document.querySelector(".timer");
 var finalScore = document.getElementById("final-score");
-
+var initialsField = document.getElementById("initials");
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+var questionsScreen = document.getElementById("questions");
+var interval;
 // Keep track of current question 
 var currentQuestionNumber = 0;
 
@@ -19,13 +23,15 @@ function clearScreen()
 // Show when game has finished 
 function endGame () 
 {
+    clearInterval(interval);
+    initialsField.innerHTML = "";
     questionsScreen.classList.add("hide");
     //Show endscreen
     endScreen.classList.remove("hide");
     //Clear content
     startScreen.textContent = "";
     // Store score
-    localStorage.setItem("highscore", time);
+    localStorage.setItem("highscore", time);  // ! CHANGE THIS, CREATE OBJECT INSTEAD
     // Remove timer
     timer.classList.add("hide");
     // Show score
@@ -35,24 +41,33 @@ function endGame ()
     }
     else 
     {
-        finalScore.textContent = "...honestly, you don't want to know.  Why not have another go?"
+        finalScore.textContent = "0 ...zero, zip, zilch, nada.  Have another try, and if you can't be right, at least be wrong quicker"
     }
 }
 
+// ! This is called when submit button is pressed.
 function storeScore (initals) {
         // Store score
-        localStorage.setItem("highscore", time);
+        console.log("time is: " + time)
+        const playerDetails = {
+            initials: "initials",
+            highscore: time
+        }
+        localStorage.setItem("initials", "highscore");
 }
 
+//  !When submit button is pressed, need to trigger function (STORESCORE) that calls (at least) 2 functions.  One that is going to.... NEED TO - Empty text field BEFORE this point actually.  Need to grab initials from the field and  create object with initials and score and store in local storage, go to highscores? CHANGE PAGE TO HIGHSCORES
 
+// ? localStorage stores key:value Pairs - need name and score. 
+
+//add event listener to submit score button
+var submitScore = document.getElementById('submit');
+submitScore.addEventListener('click', storeScore("abc"));
 
 //initialise timer
 var time = 75;
 
-// store sounds as variables
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
-var questionsScreen = document.getElementById("questions");
+
 
 function renderQuestion(index) {
     if (currentQuestionNumber >= questions.length) {
@@ -90,13 +105,14 @@ timeDisplay.textContent = (time);
 // Keep track of time
 function myTimer()
 {
-    var interval = setInterval(function () {
+    interval = setInterval(function () {
         if (time > 0) {
             time--;
             timeDisplay.textContent = time;
         }
         else {
             clearInterval(interval);
+            endGame();
         }
     }, 1000)
 }
@@ -108,7 +124,8 @@ function startButtonClicked() {
 }
 
 // Check if answer is correct and return boolean
-function isCorrect (answer) {
+function isCorrect (answer) 
+{
     if (answer === questions[currentQuestionNumber].answer)
     {
         return true;
@@ -118,6 +135,8 @@ function isCorrect (answer) {
         return false;
     }
 }
+
+// Process answer and inform user whether correct or incorrect 
 function handleAnswer (event) 
 {
     var result = document.createElement("p");
